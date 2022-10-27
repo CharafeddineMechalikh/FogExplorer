f7918026a42549fcc502ba25dc2a3b32cb2451cf
Charafeddine Mechalikh <46229052+CharafeddineMechalikh@users.noreply.github.com>
2022-10-27 14:14:18 +0100

    Deleted commented-out code

--- test/evaluator-test.js
+++ test/evaluator-test.js
@@ -51,33 +51,3 @@ describe("Evaluator (load assigned splitAndMergeExample", function () {
     });
 });
 
-// TODO: RUN TEST WITH SECOND DATA SETd
-// describe("Evaluator (simpleExample)", function () {
-//     const parsedJSON = require('../exampleData/simpleExample');
-//     const evaluator = require("../sharedLogic/evaluator");
-//
-//     it("should have loaded its modules", function () {
-//         const success = evaluator.loadModels(parsedJSON);
-//         assert.equal(success, true, "Models have not been loaded correctly.");
-//     });
-//
-//     it("should have calculated a coherent flow with the correct metrics.", function () {
-//         evaluator.setCurrentlySelectedModuleToModuleWithId("sm_1");
-//         evaluator.assignCurrentlySelectedModuleToNodeWithId("e1");
-//         evaluator.setCurrentlySelectedModuleToModuleWithId("sm_2");
-//         evaluator.assignCurrentlySelectedModuleToNodeWithId("e2");
-//         evaluator.setCurrentlySelectedModuleToModuleWithId("fm");
-//         evaluator.assignCurrentlySelectedModuleToNodeWithId("c");
-//         evaluator.setCurrentlySelectedModuleToModuleWithId("am_1");
-//         evaluator.assignCurrentlySelectedModuleToNodeWithId("e3");
-//         evaluator.setCurrentlySelectedModuleToModuleWithId("am_2");
-//         evaluator.assignCurrentlySelectedModuleToNodeWithId("e4");
-//
-//         evaluator.calculateCoherentFlow();
-//
-//         assert.equal(evaluator.getTotalProcessingCost(), 100, "Processing cost is not as expected.");
-//         assert.equal(evaluator.getTotalProcessingTime(), 8, "Processing time is not as expected.");
-//         assert.equal(evaluator.getTotalTransmissionCost(), 42, "Transmission cost is not as expected.");
-//         assert.equal(evaluator.getTotalTransmissionTime(), 5, "Transmission time is not as expected.");
-//     });
-// });
